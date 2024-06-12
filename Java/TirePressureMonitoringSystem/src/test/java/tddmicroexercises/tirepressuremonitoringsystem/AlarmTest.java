5708c6bcd0ff95c42a8edc6c29634624d873459a
mathys.caronpro@gmail.com <tysma.caron84@gmail.com>
2024-06-12 18:48:26 +0200

    testAlarmPressure

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -11,4 +11,28 @@ public class AlarmTest {
         Alarm alarm = new Alarm();
         assertFalse(alarm.isAlarmOn());
     }
+
+    @Test
+    public void testAlarmPressureIsTooLow() {
+        //WHEN
+        when(mockSensor.popNextPressurePsiValue()).thenReturn(16.0);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void testAlarmPressureIsTooHigh() {
+        //WHEN
+        when(mockSensor.popNextPressurePsiValue()).thenReturn(22.0);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void testAlarmPressureIsNormal() {
+        //WHEN
+        when(mockSensor.popNextPressurePsiValue()).thenReturn(18.0);
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
 }
